aaa44551eaa4b37e7e0cd509207152b522881a60
w4wycliff <w4wycliff@gmail.com>
2020-02-16 00:48:14 +0300

    add project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout>
+        Hello World
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
